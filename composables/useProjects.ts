a8285bc236b4fd8a63ce1a0826456a1ca018a52c
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-09-26 11:03:31 -0300

    wip

--- composables/useProjects.ts
+++ composables/useProjects.ts
@@ -1,43 +1,58 @@
-
 type Project = {
   id: number;
   name: string;
   description: string;
   skills: string[];
-  image: string[];  
-}
+  knowledgeAreas: string[];
+  image: string;
+};
 
 export default function useProjects() {
   return [
     {
       id: 1,
       name: "Seminário IoT",
-      description: "Seminário sobre Internet das Coisas, com desenvolvimento de site HTML5 e CSS3.",
+      description:
+        "Seminário sobre Internet das Coisas, com desenvolvimento de site HTML5 e CSS3.",
+      skills: ["HTML5", "CSS3"],
+      knowledgeAreas: ["Front-end", "Web Design"],
     },
     {
       id: 2,
       name: "Macieuls Coffee: Flyer da cafeteria",
-      description: "Flyer criado no Corel Draw para a cafeteria Macieuls Coffee.",
+      description:
+        "Flyer criado no Corel Draw para a cafeteria Macieuls Coffee.",
+      skills: ["Corel Draw"],
+      knowledgeAreas: ["Design Gráfico"],
     },
     {
       id: 3,
-      name: "Calculadora científica responsiva", 
-      description: "Calculadora científica responsiva criada com HTML, CSS e JavaScript.",
+      name: "Calculadora científica responsiva",
+      description:
+        "Calculadora científica responsiva criada com HTML, CSS e JavaScript.",
+      skills: ["HTML5", "CSS3", "JavaScript"],
+      knowledgeAreas: ["Front-end", "Web Design"],
     },
     {
       id: 4,
       name: "E-commerce com JQuery",
       description: "E-commerce criado com JQuery e API de produtos.",
+      skills: ["JQuery"],
+      knowledgeAreas: ["Front-end", "Web Design"],
     },
     {
       id: 5,
       name: "Aplicação de Livraria",
       description: "Aplicação de livraria com Ionic e NuxtJS.",
+      skills: ["Ionic", "NuxtJS", "Tailwind CSS"],
     },
     {
       id: 6,
       name: "Sistema de login com Laravel e Docker",
-      description: "Arquitetura orquestrada com Docker, utilizando Laravel e MySQL.",
-    }
+      description:
+        "Arquitetura orquestrada com Docker, utilizando Laravel e MySQL.",
+      skills: ["Laravel", "Docker", "MySQL"],
+      knowledgeAreas: ["Back-end", "DevOps"],
+    },
   ] as Project[];
 }
--- pages/project/[id].vue
+++ pages/project/[id].vue
@@ -1,18 +1,41 @@
 <script setup lang="ts">
 const route = useRoute();
 const project = useProjects().find((project) => project.id === Number(route.params.id)) || {
-  id: 0,
-  name: 'Projeto não encontrado',
-  description: 'O projeto que você está tentando acessar não existe.',
-  skills: [],
+  id: 1,
+  name: "Seminário IoT",
+  description:
+    "Seminário sobre Internet das Coisas, com desenvolvimento de site HTML5 e CSS3.",
+  skills: ["HTML5", "CSS3"],
+  knowledgeAreas: ["Front-end", "Web Design"],
 };
 </script>
 <template>
   <div class="">
+    <button class="mb-6 -ml-1 rounded-lg btn border-neutral-content" @click="$router.back()">
+      Voltar
+    </button>
     <h1 class="text-2xl font-bold">{{ project.name }} - {{ project.id }}° Semestre</h1>
-    <p class="mt-4 font-thin">{{ project.description }}</p>
+    <p class="mt-2 font-thin">{{ project.description }}</p>
     <div class="divider divider-primary"></div>
-    <p></p> <!-- Skills -->
+    <div class="flex flex-col gap-4 mt-4">
+      <div class="">
+        <h2 class="text-xl font-bold">Áreas de conhecimento abordadas:</h2>
+        <ol class="flex gap-4 mt-2">
+          <li v-for="(area, index) in project.knowledgeAreas" :key="index">
+            <div class="p-4 badge badge-primary badge-outline rounded-xl">{{ area }}</div>
+          </li>
+        </ol>
+      </div>
+      <div class="">
+        <h2 class="text-xl font-bold">Tecnologias usadas:</h2>
+        <ol class="flex gap-4 mt-2">
+          <li v-for="(skill, index) in project.skills" :key="index">
+            <div class="p-4 badge badge-secondary badge-outline rounded-xl">{{ skill }}</div>
+          </li>
+        </ol>
+      </div>
+    </div>
+    
   </div>
 </template>
 <style scoped></style>
