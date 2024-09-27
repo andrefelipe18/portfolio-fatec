5d67f22bbe05e71b4ec0638d4dbdd63fdc29d90f
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-09-27 19:29:40 -0300

    wip

--- components/About/Image.vue
+++ components/About/Image.vue
@@ -3,6 +3,13 @@
 </script>
 <template>
 	<section id="image" class="flex justify-center mt-6">
-		<NuxtImg src="/imgs/me.jpg" alt="me" class="max-w-[34rem] w-full rounded-xl" />
-	</section>
+    <NuxtImg 
+      src="/imgs/me.jpg" 
+      alt="me" 
+      class="max-w-[34rem] w-full rounded-xl" 
+      width="544" 
+      height="408" 
+      format="webp"
+    />
+  </section>
 </template>
