cd0f09984a69a5d19ae6bfc016643045abffa569
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-11-10 09:21:42 -0300

    wip

--- components/Resume/ProfessionalExperience.vue
+++ components/Resume/ProfessionalExperience.vue
@@ -6,8 +6,20 @@
 		<h2 class="text-2xl font-bold">Experiência Profissional: </h2>
 		<div class="flex flex-col gap-2 mt-2 ml-2">
 			<div class="flex flex-col">
+				<h3 class="text-lg font-semibold">Desenvolvedor FullStack Pleno</h3>
+				<p class="text-sm">Outubro 2024 - Momento</p>
+				<p class="flex items-center mt-1 text-sm">
+					<Icon :name="'mingcute:diamond-2-line'" />
+					<span class="ml-2">
+						Desenvolvedor responsável pelo desenvolvimento e gerenciamento das principais funcionalidades do backend,
+						incluindo a construção e manutenção do Painel Admin e API. Atuo também na integração e otimização de
+						funcionalidades no frontend.
+					</span>
+				</p>
+			</div>
+			<div class="flex flex-col">
 				<h3 class="text-lg font-semibold">Estagiário de desenvolvimento</h3>
-				<p class="text-sm">Março 2023 - Momento</p>
+				<p class="text-sm">Março 2023 - Dez 2024</p>
 				<p class="flex items-center mt-1 text-sm">
 					<Icon :name="'mingcute:diamond-2-line'" />
 					<span class="ml-2">Desenvolvimento de aplicações web</span>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const {getAll} = useProjects();
+const { getAll } = useProjects();
 
 const projects = getAll();
 
@@ -10,7 +10,8 @@ definePageMeta({
 <template>
   <div class="flex flex-col gap-6 my-auto">
     <h2 class="text-2xl">Projetos:</h2>
-    <NuxtLink v-for="(project, index) in projects" :to="`project/${project.id}`" class="p-6 transition-all border rounded-lg cursor-pointer border-primary hover:transform hover:scale-105 group">
+    <NuxtLink v-for="(project, index) in projects" :to="`project/${project.id}`"
+      class="p-6 transition-all border rounded-lg cursor-pointer border-primary hover:transform hover:scale-105 group">
       <p class="flex items-center gap-2 text-[1.10rem] font-bold group group-hover:text-primary transition">
         <span>{{ project.name }}</span>
       </p>
@@ -18,9 +19,39 @@ definePageMeta({
       <div class="flex justify-between mt-2 text-sm">
         <p>{{ project.id }}° semestre </p>
         <div class="">
-
         </div>
       </div>
     </NuxtLink>
+
+    <h2 class="text-2xl">
+      Iniciações Científicas:
+    </h2>
+
+    <div class="p-6 transition-all border rounded-lg border-primary hover:transform hover:scale-105 group">
+      <p class="flex items-center gap-2 text-[1.10rem] font-bold group group-hover:text-primary transition">
+        <span>
+          Desenvolvimento de Ambiente Virtual em um Site Web para o Auxílio do Desenvolvimento da Visão Espacial
+        </span>
+      </p>
+      <div class="flex justify-between mt-2 text-sm">
+        <p>1 e 2° semestre </p>
+        <div class="">
+        </div>
+      </div>
+    </div>
+
+    <div class="p-6 transition-all border rounded-lg border-primary hover:transform hover:scale-105 group">
+      <p class="flex items-center gap-2 text-[1.10rem] font-bold group group-hover:text-primary transition">
+        <span>
+          Smart Home Control: Desenvolvimento do back-end de um sistema web com AdonisJS e MySQL para gerenciamento
+          e integração de dispositivos inteligentes.
+        </span>
+      </p>
+      <div class="flex justify-between mt-2 text-sm">
+        <p>4° semestre </p>
+        <div class="">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
